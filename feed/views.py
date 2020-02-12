2ff5343e2550e29c5197e560b6cc0f084fde345e
Andreas Amundsen <andramu@stud.ntnu.no>
2020-02-12 10:46:08 +0100

    Define view function for feed application

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .models import Exercise
+
 # Create your views here.
+def home(request):
+
+    latestExercises = Exercise.objects.all()
+
+    context = {
+        'exercises': latestExercises
+    }
+
+    return render(request, 'feed/search.html', context)
