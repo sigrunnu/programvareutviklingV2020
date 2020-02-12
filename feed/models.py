0c4f4660b9f50246382809db3b8c50f9a7d1833d
Andreas Amundsen <andramu@stud.ntnu.no>
2020-02-12 10:10:56 +0100

    Add muscle group entity to database model

--- feed/models.py
+++ feed/models.py
@@ -1,22 +1,19 @@
 from django.db import models
 
 
+class MuscleGroup(models.Model):
+    muscleGroupTitle = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.muscleGroupTitle
+
+
 class Exercise(models.Model):
     exerciseTitle = models.CharField(max_length=200)
     exerciseDescription = models.CharField(max_length=500)
     pub_date = models.DateTimeField('date published')
 
+    # muscleGroup = models.ForeignKey(MuscleGroup, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.exerciseTitle
-
-
-""""
-class (models.Model):
-    question = models.ForeignKey(Exercise, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-"""
-
-
-class MuscleGroup(models.Model):
-    muscleGroupTitle = models.CharField(max_length=200)
