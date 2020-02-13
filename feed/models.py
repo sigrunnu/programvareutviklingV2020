552977cbcd1c88835c1862ce8bd3eec43a4e4aa6
Åse Håtveit <aasehaa@stud.ntnu.no>
2020-02-13 15:01:39 +0100

    lots of code added in views. Managed to fetch object from database(seen in terminal), but not added on html page yet

--- feed/models.py
+++ feed/models.py
@@ -15,5 +15,7 @@ class Exercise(models.Model):
 
     muscleGroup = models.ManyToManyField(MuscleGroup)
 
+
+
     def __str__(self):
         return self.exerciseTitle
--- feed/views.py
+++ feed/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 from .models import Exercise
-
+#testing search
+import sqlite3
+from sqlite3 import Error
 
 # Create your views here.
 def home(request):
@@ -24,4 +26,80 @@ def search(request):
     """
     search_word = request.GET['search_field']
     print(search_word)
+
+    #testing search
+    conn = create_connection('db.sqlite3')
+    select_all_tasks(conn, search_word)
+
+
     return render(request, 'feed/search.html', {'search_content': search_word})
+
+
+# testing search
+def create_connection(db_file):
+        """ create a database connection to the SQLite database
+            specified by the db_file
+        :param db_file: database file
+        :return: Connection object or None
+        """
+        conn = None
+        try:
+            conn = sqlite3.connect(db_file)
+        except Error as e:
+            print(e)
+
+        return conn
+
+#fetching data from feed_exercise
+def select_all_tasks(conn, search_word):
+        """
+        Query all rows in the tasks table
+        :param conn: the Connection object
+        :return:
+        """
+        cur = conn.cursor()
+        #cur.execute("SELECT * FROM feed_exercise")
+        cur.execute("SELECT * FROM feed_exercise WHERE exerciseTitle=?", (search_word,))
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+
+
+
+'''
+Not in use yet
+    tester for bare select_all i første omgang - her går de etter priority som er definert som tall
+    def select_task_by_priority(conn, priority):
+        """
+        Query tasks by priority
+        :param conn: the Connection object
+        :param priority:
+        :return:
+        """
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM tasks WHERE priority=?", (priority,))
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+
+
+def main(self):
+        database = r"C:\sqlite\db\pythonsqlite.db"
+
+        # create a database connection
+        conn = create_connection(database)
+        with conn:
+            #tester for bare select_all
+            #print("1. Query task by priority:")
+            #select_task_by_priority(conn, 1)
+
+            print("2. Query all exercises")
+            select_all_tasks(conn)
+
+if __name__ == '__main__':
+        main()
+'''
