108cd4cc2c419ec379465637310d749264d6c25e
Andreas Amundsen <andramu@stud.ntnu.no>
2020-02-17 09:47:28 +0100

    Add more tests

--- feed/tests/test_models.py
+++ feed/tests/test_models.py
@@ -7,3 +7,19 @@ class TestModels:
     def test_exercise_title_not_empty(self):
         exercise = mixer.blend('feed.Exercise', exerciseTitle="Bench press")
         assert len(exercise.exerciseTitle) > 0
+
+    def test_description_not_empty(self):
+        exercise = mixer.blend('feed.Exercise',
+                               exerciseDescription="Some description")
+        assert len(exercise.exerciseDescription) > 0
+
+    def test_exercise_title_is_correct(self):
+        title = "Bicep curl"
+        exercise = mixer.blend('feed.Exercise', exerciseTitle=title)
+        assert title == exercise.exerciseTitle
+
+    def test_description_is_correct(self):
+        description = "Some description"
+        exercise = mixer.blend('feed.Exercise',
+                               exerciseDescription=description)
+        assert description == exercise.exerciseDescription
