8b6f98c8063c256824db119b99f16415fd1cab0e
Andreas Amundsen <andramu@stud.ntnu.no>
2020-02-17 10:08:53 +0100

    Revert "Merge branch 'feat/headerAndFooter' into 'master'"
    This reverts merge request !10

--- feed/models.py
+++ feed/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models import Q
 
 
 class MuscleGroup(models.Model):
@@ -8,38 +7,13 @@ class MuscleGroup(models.Model):
     def __str__(self):
         return self.muscleGroupTitle
 
-    @staticmethod
-    def get_queryset_by_search_word(search_word):
-        """
-        :param search_word: the word that shall be searched
-        :type search_word: String
-        :return: All the objects that contains the search word in
-        muscleGroupTitle
-        :rtype: QuerySet
-        """
-        return MuscleGroup.objects.filter(
-            Q(muscleGroupTitle__icontains=search_word))
-
 
 class Exercise(models.Model):
     exerciseTitle = models.CharField(max_length=200)
     exerciseDescription = models.CharField(max_length=500)
     pub_date = models.DateTimeField('date published')
 
-    muscleGroup = models.ManyToManyField(MuscleGroup, blank=True)
+    muscleGroup = models.ManyToManyField(MuscleGroup)
 
     def __str__(self):
         return self.exerciseTitle
-
-    @staticmethod
-    def get_queryset_by_search_word(search_word):
-        """
-        :param search_word: the word that shall be searched
-        :type search_word: String
-        :return: All the objects that contains the search word in
-        exerciseTitle or exerciseDescription
-        :rtype: QuerySet
-        """
-        return Exercise.objects.filter(
-            Q(exerciseDescription__icontains=search_word)
-            | Q(exerciseTitle__icontains=search_word))
--- feed/tests/test_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import reverse, resolve
-
-
-class TestUrls:
-    def test_search_url(self):
-        path = reverse('search')
-        assert resolve(path).view_name == 'search'
