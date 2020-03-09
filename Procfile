1d7ed19ac2295d03c9a031a823e02374f440dd4f
hansmja <hansmja@stud.ntnu.no>
2020-03-09 13:29:05 +0100

    Hotfix

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web:gunicorn exerciseit.wsgi --log-file -
+web: gunicorn exerciseit.wsgi:application --log-file - --log-level debug
+python manage.py collectstatic --noinput
+manage.py migrate
--- exerciseit/settings.py
+++ exerciseit/settings.py
@@ -161,21 +161,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-django_heroku.settings(locals())
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 try:
     from .local_settings import *
