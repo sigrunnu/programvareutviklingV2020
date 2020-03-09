f79de46343c48db39e79f5ab6ad911648b47a7bc
hansmja <hansmja@stud.ntnu.no>
2020-03-09 14:45:11 +0100

    Hotfix pycodestyle

--- exerciseit/wsgi.py
+++ exerciseit/wsgi.py
@@ -15,4 +15,3 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exerciseit.settings')
 application = get_wsgi_application()
 application = WhiteNoise(application, root='')
-
