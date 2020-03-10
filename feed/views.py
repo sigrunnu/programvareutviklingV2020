3b8e83d0d77ffc9a3a61c27dfe74a9a3572e6ef5
hansmja <hansmja@stud.ntnu.no>
2020-03-10 20:03:38 +0100

    Added todo in feed/view

--- feed/views.py
+++ feed/views.py
@@ -50,6 +50,7 @@ def search(request):
 
     print(result.to_dict())
 
+    # TODO: Make the result index fetch the relevant Exercises from the db
     context = {
         'exercises': result
     }
