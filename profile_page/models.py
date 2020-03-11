ba4acd8b706c883620c7c30f11ad0a0a0d100ccd
Åse Håtveit <aasehaa@stud.ntnu.no>
2020-03-11 11:16:02 +0100

    pycodestyle fix

--- profile_page/models.py
+++ profile_page/models.py
@@ -7,30 +7,34 @@ from six import python_2_unicode_compatible
 
 @python_2_unicode_compatible
 class User(models.Model):
-    userName = models.CharField(
+    userName=models.CharField(
         max_length=50,
         verbose_name='Username'
     )
     email = models.CharField(
-        max_length = 100,
+        max_length=100,
         verbose_name='Email'
     )
     age = models.IntegerField(
         default=0,
-        verbose_name = 'Age'
+        verbose_name='Age'
     )
     password = models.CharField(
         max_length=32,
-        widget=forms.PasswordInput #usikker her?
+        widget=forms.PasswordInput
     )
     sex = models.CharField(
         max_length=50,
         null=True,
         blank=True
     )
+    pro = models.CheckboxInput(
+        default=false,
+        widget=forms.CheckboxInput
+    )
 
     class Meta(object):
-        ordering = ["userName", "email", "age", "password", "sex"]
+        ordering=["userName", "email", "age", "password", "sex"]
 
     def __str__(self):
         return self.userName
