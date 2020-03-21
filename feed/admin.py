f4aecce709c9da2779c7b775d2fa5383238c8363
hansmja <hansmja@stud.ntnu.no>
2020-03-21 14:41:49 +0100

    Added rating score and number of favorisations to admin page

--- feed/admin.py
+++ feed/admin.py
@@ -1,22 +1,31 @@
 from django.contrib import admin
 from image_cropping import ImageCroppingMixin
-from .models import Exercise
-from .models import MuscleGroup, Favorisation
+from .models import MuscleGroup, Favorisation, Rating, Exercise
 from profile_page.models import CreatedBy
 
 
-class ExerciseAdmin(ImageCroppingMixin,
-                    admin.ModelAdmin):
-    pass
+class ExerciseAdmin(ImageCroppingMixin, admin.ModelAdmin):
+    filter_horizontal = ('muscle_group',)
+    list_display = (
+        "exercise_title",
+        "pub_date",
+        "rating_score",
+        "number_of_favorisations",
+        "is_public",
+        "created_by"
+    )
 
-    filter_horizontal = ('muscleGroup',)
+    @staticmethod
+    def rating_score(obj):
+        return obj.get_rating_score()
+
+    @staticmethod
+    def number_of_favorisations(obj):
+        return obj.get_number_of_favorisations()
+
     admin.site.register(MuscleGroup)
-
-    def save_model(self, request, obj, form, change):
-        if not change:
-            obj.createdBy = CreatedBy.objects.get(user=request.user)
-            obj.save()
 
 
 admin.site.register(Exercise, ExerciseAdmin)
 admin.site.register(Favorisation)
+admin.site.register(Rating)
