e2b6d20049553a2df3eb5a979c7356e1725f4598
hansmja <hansmja@stud.ntnu.no>
2020-03-21 15:31:10 +0100

    Made all super users professional users

--- profile_page/models.py
+++ profile_page/models.py
@@ -33,10 +33,16 @@ class CreatedBy(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
-        CreatedBy.objects.create(user=instance)
+        if instance.is_superuser:
+            Profile.objects.create(user=instance, is_pro=True)
+            CreatedBy.objects.create(user=instance)
+        else:
+            Profile.objects.create(user=instance)
+            CreatedBy.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
+    if instance.is_superuser:
+        instance.profile.is_pro = True
     instance.profile.save()
