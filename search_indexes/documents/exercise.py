0088fb7604f608418e1140d37d52304204d02112
hansmja <hansmja@stud.ntnu.no>
2020-03-22 19:09:43 +0100

    fixed search index error

--- search_indexes/documents/exercise.py
+++ search_indexes/documents/exercise.py
@@ -16,7 +16,7 @@ INDEX.settings(
 html_strip = analyzer(
     'html_strip',
     tokenizer="standard",
-    filter=["standard", "lowercase", "stop", "snowball"],
+    filter=["lowercase", "stop", "snowball"],
     char_filter=["html_strip"]
 )
 
