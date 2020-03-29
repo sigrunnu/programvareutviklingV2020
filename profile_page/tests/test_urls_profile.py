36d1aeb68615e7f9f4197db231eee799392d6e6a
Konrad Thorsen <konradvt@stud.ntnu.no>
2020-03-29 17:22:57 +0200

    Added "non-working" test TestUrls

---
+++ profile_page/tests/test_urls_profile.py
@@ -0,0 +1,19 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from profile_page.views import profile, signupView, LoginView
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_profile_is_resolved(self):
+        url = reverse('profile')
+        self.assertEqual(resolve(url).func, profile)
+
+    # def test_signupView_is_resolved(self):
+    #     url = reverse('signup')
+    #     self.assertEqual(resolve(url).func, signupView)
+    #
+    # def test_loginView_is_resolved(self):
+    #     url = reverse('login')
+    #     self.assertEqual(resolve(url).func.view_class, LoginView)
